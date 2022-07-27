9156c2db85ff737f8faa039f04814fac7e3bafce
surafelfa <surafelfa@gmail.com>
2022-07-27 14:45:48 +0300

    code optimized

--- Basic Algorithm/titleCase.js
+++ Basic Algorithm/titleCase.js
@@ -1,13 +1,6 @@
 function titleCase(str) {
-
-    let arr = str.toLowerCase().split(' '), arr2;
-    for(let i = 0; i < arr.length; i++){
-      arr2 = arr[i].split('');
-      arr2[0] = arr2[0].toUpperCase();
-      arr[i] = arr2.join('');
-    }
-    //console.log(arr.join(' '));
-    return arr.join(' ');
+  return str.toLowerCase()
+  .replace(/(^|\s)\S/g, char => char.toUpperCase());
   }
   
   titleCase("sHoRt AnD sToUt");
