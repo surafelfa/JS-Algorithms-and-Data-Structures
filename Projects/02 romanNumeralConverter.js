46154524c47785470bc6951a25bd88af5b8f0946
surafelfa <surafelfa@gmail.com>
2023-09-19 14:25:44 +0300

    roman numberal converter is updated

--- Projects/02 romanNumeralConverter.js
+++ Projects/02 romanNumeralConverter.js
@@ -1,65 +1,45 @@
 const arabicToRoman = {
-    1: 'I',
-    2: 'II',
-    3: 'III',
-    4: 'IV',
-    5: 'V',
-    6: 'VI',
-    7: 'VII',
-    8: 'VIII',
-    9: 'IX',
-    10: 'X',
-    20: 'XX',
-    30: 'XXX',
-    40: 'XL',
-    50: 'L',
-    60: 'LX',
-    70: 'LXX',
-    80: 'LXXX',
-    90: 'XC',
-    100: 'C',
-    200: 'CC',
-    300: 'CCC',
-    400: 'CD',
-    500: 'D',
-    600: 'DC',
-    700: 'DCC',
-    800: 'DCCC',
-    900: 'CM',
-    1000: 'M',
-    2000: 'MM',
-    3000: 'MMM',
-  }
-  //returns the number of digits
-  function numLength(number){
-    let length = 0;
-    while(number != 0){
-      number = parseInt(number/10);
-      ++length;
-    }
-    //console.log(length);
-    return length;
-  }
-  
-  function addZeros(length){
-    let zeros = 1;
-    for(let i = 1; i < length; i++){
-      zeros *= 10;
-    }
-    //console.log(zeros);
-    return zeros;
-  }
-  function convertToRoman(number) {
-    let toRoman = '', value;
-    for(let i = numLength(number); i > 0; i--){
-      value  = (parseInt(number/addZeros(i))) * addZeros(i);
-      if(value != 0){
-        toRoman += arabicToRoman[value];
-      }
-      number = number - value;
-    }
-    //console.log(toRoman);
-    return toRoman;
-  }
-  
-  convertToRoman(68);
+  1: "I",
+  2: "II",
+  3: "III",
+  4: "IV",
+  5: "V",
+  6: "VI",
+  7: "VII",
+  8: "VIII",
+  9: "IX",
+  10: "X",
+  20: "XX",
+  30: "XXX",
+  40: "XL",
+  50: "L",
+  60: "LX",
+  70: "LXX",
+  80: "LXXX",
+  90: "XC",
+  100: "C",
+  200: "CC",
+  300: "CCC",
+  400: "CD",
+  500: "D",
+  600: "DC",
+  700: "DCC",
+  800: "DCCC",
+  900: "CM",
+  1000: "M",
+  2000: "MM",
+  3000: "MMM",
+};
+function convertToRoman(number) {
+  const numbers = number.toString().split("");
+  let lastIndex = numbers.length - 1;
+  let roman = "";
+  let arabic;
+  numbers.forEach((element) => {
+    arabic = Math.pow(10, lastIndex) * element;
+    if (arabic !== 0) roman += arabicToRoman[arabic];
+    --lastIndex;
+  });
+  return roman;
+}
+module.exports = convertToRoman;
