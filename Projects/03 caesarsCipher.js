f5b37d1956b103fdc64a808bb73d8cf9212b2a97
surafelfa <surafelfa@gmail.com>
2023-09-19 15:46:16 +0300

    ROT13 is updated

--- Projects/03 caesarsCipher.js
+++ Projects/03 caesarsCipher.js
@@ -1,19 +1,18 @@
-const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let charIndex;
-function rot13(str) {
-  let arr = str.split('');
-  for(let i = 0; i < arr.length; i++){
-    if(/[^A-Z]/.test(arr[i])){
-      //do noting if char at index i is different from A-Z.
-    }else{
-      charIndex = alphabets.indexOf(arr[i])+13;
-      if(charIndex > 25){
-        charIndex -=26
-      }
-      arr[i] = alphabets.charAt(charIndex);
+function ROT13(encoded) {
+  let decoded = "",
+    char,
+    ASCII;
+  for (let i = 0; i < encoded.length; i++) {
+    char = encoded.charAt(i);
+    if (/[A-Z]/.test(char)) {
+      ASCII = encoded.charCodeAt(i);
+      ASCII <= 77
+        ? (decoded += String.fromCharCode(ASCII + 13))
+        : (decoded += String.fromCharCode(ASCII - 13));
+    } else {
+      decoded += char;
     }
   }
-  //console.log(arr.join(''));
-  return arr.join('');
+  return decoded;
 }
-rot13("SERR PBQR PNZC");
+module.exports = ROT13;
