762b6aa8eb9bec1a10770ec58ef8249501aad884
surafelfa <surafelfa@gmail.com>
2023-09-19 14:16:07 +0300

    palindromeChecker is updated

--- Projects/01 palindromeChecker.js
+++ Projects/01 palindromeChecker.js
@@ -1,11 +1,7 @@
-function palindrome(str) {
-    let alphanumericString = str.replace(/[^A-z0-9]+|_+/g,"");
-    let lowerCaseString = alphanumericString.toLowerCase();
-    let pal = true;
-    for(let i = 0, j = lowerCaseString.length-1; i < j && pal; i++, j--){
-      if(lowerCaseString[i] !== lowerCaseString[j]){
-        pal = false;
-      }
-    }
-    return pal;
-  }
+function isPalindrome(string) {
+  const newString = string.toLowerCase().replace(/[\W_]+/g, "");
+  const revString = [...newString].reverse().join("");
+  return newString == revString;
+}
+
+module.exports = isPalindrome;
